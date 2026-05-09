95b9badb871302bc34e28ec96eee6d54997556cf
adriatic <nik@congral.us>
2026-05-09 10:55:43 -0700

    Add create-pact-production.sh script

---
+++ create-pact-production.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# create-pact-production.sh
+# Copies pact-react-v2 source files to pact-production (clean rebuild target)
+# Excludes: .git, .DS_Store, *.tsbuildinfo, out/, pact-data/, host/, betas/
+
+SRC="$HOME/Work/pact-react-v2"
+DEST="$HOME/Work/pact-production"
+
+echo "Removing existing pact-production..."
+rm -rf "$DEST"
+
+echo "Creating fresh pact-production..."
+mkdir -p "$DEST"
+
+echo "Copying source files..."
+rsync -av \
+  --exclude='.git' \
+  --exclude='.DS_Store' \
+  --exclude='*.tsbuildinfo' \
+  --exclude='out/' \
+  --exclude='pact-data/' \
+  --exclude='host/' \
+  --exclude='betas/' \
+  "$SRC/" "$DEST/"
+
+echo ""
+echo "Done. pact-production is ready to build."
+echo "Next: cd $DEST && npm install && npm run build"
